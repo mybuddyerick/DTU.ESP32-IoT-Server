e74d9fe6b024f5a6681d8537c27396a4d7a74c29
MyBuddyErick <eirik.daniels@gmail.com>
2025-04-04 11:21:23 +0200

    main.py contains pin locations

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 from server import *
 from mcp9808 import *
+
+'''
+
+PIN Location
+
+###############
+# Temp Sensor #
+#   SCL - 22  #
+#   SDA - 23  #
+###############
+
+#################
+# Potentiometer #
+#      12       #
+#################
+
+#############
+#  RGB LED  #
+#   red 27  #
+# green 15  #
+#  blue 32  #
+#############
+'''
